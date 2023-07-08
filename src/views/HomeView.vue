e4dc21468619c3a3e6933989457980474bba0eee
callmecposu <pc.cposu@gmail.com>
2023-07-08 18:11:49 -0400

    add bootstrap & home-intro section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+
+    <!-- intro section -->
+    <section id="intro">
+      <div class="container-lg">
+        <div class="row justify-content-center align-items-center">
+          <div class="col-md-5 text-center text-md-start">
+            <h1>
+              <div class="display-2 border-bottom border-5 rounded">
+                Your personal Note Pal
+              </div>
+              <div class="display-6 text-muted mt-2">As if you needed one</div>
+            </h1>
+          </div>
+          <div class="col-md-5 text-center m-3">
+            <img class="img-fluid" src="../assets/notes.png" alt="blank notes">
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+};
 </script>
+
+<style scoped>
+section {
+  padding: 60px 0;
+}
+</style>
