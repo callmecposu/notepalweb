84a966c158a1ee0f7f42dd73f6a29bc6dc3b8eda
callmecposu <pc.cposu@gmail.com>
2023-07-08 19:27:09 -0400

    add navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,48 @@
 <template>
   <div class="home">
-    
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-md bg-light">
+      <div class="container-xxl">
+        <a href="#intro" class="navbar-brand">
+          <img class="mx-2" src="../assets/logo.png" width="50" alt="logo" />
+          <span class="lead fw-bold">NotePal</span>
+        </a>
+        <!-- toggle btn for mobiles -->
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#main-nav"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          class="collapse navbar-collapse justify-content-end align-center"
+          id="main-nav"
+        >
+          <ul class="navbar-nav">
+            <li class="nav-item d-block d-md-none">
+              <div class="container-fluid border-top border-5"></div>
+            </li>
+            <li class="nav-item mx-3">
+              <a href="#" class="nav-link">About</a>
+            </li>
+            <li class="nav-item mx-3 d-none d-md-block">
+              <a href="#" class="btn btn-outline-primary">Login</a>
+            </li>
+            <li class="nav-item d-block d-md-none">
+              <a href="#" class="nav-link link-primary">Login</a>
+            </li>
+            <li class="nav-item mx-3 d-none d-md-block">
+              <a href="#" class="btn btn-success">Sign Up</a>
+            </li>
+            <li class="nav-item d-block d-md-none">
+              <a href="#" class="nav-link link-success">Sign Up</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 
     <!-- intro section -->
     <section id="intro">
@@ -15,7 +57,11 @@
             </h1>
           </div>
           <div class="col-md-5 text-center m-3">
-            <img class="img-fluid" src="../assets/notes.png" alt="blank notes">
+            <img
+              class="img-fluid"
+              src="../assets/notes.png"
+              alt="blank notes"
+            />
           </div>
         </div>
       </div>
