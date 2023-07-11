6d9169a17a2a2f7abd288111fe71eeeed8894cc9
callmecposu <pc.cposu@gmail.com>
2023-07-10 21:45:34 -0400

    add a signup form

---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="d-flex align-items-center" style="height: 95vh">
+    <div class="container col-lg-6 col-10 p-5 rounded-4 border border-3">
+      <div class="lead fw-bold">NotePal</div>
+      <div class="mute mb-3">Get Started with us today!</div>
+      <div class="form-floating mb-3">
+        <input
+          v-model="username"
+          type="email"
+          class="form-control"
+          id="username"
+          placeholder="Username"
+        />
+        <label for="username">Username</label>
+      </div>
+      <div class="form-floating mb-3">
+        <input
+          v-model="password"
+          type="password"
+          class="form-control"
+          id="password"
+          placeholder="Password"
+        />
+        <label for="password">Password</label>
+      </div>
+      <div class="form-floating">
+        <input
+          v-model="confPassword"
+          type="password"
+          class="form-control"
+          id="confPassword"
+          placeholder="Confirm Password"
+        />
+        <label for="confPassword">Confirm Password</label>
+      </div>
+      <button class="btn btn-success mt-3">Sign Up</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+      confPassword: "",
+    };
+  },
+};
+</script>
+
+<style></style>
--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,10 +1,16 @@
 <template>
-  <h1>This is supposed to be a sign up view</h1>
+  <!-- <h1>This is supposed to be a sign up view</h1> -->
+  <SignUpForm/>
 </template>
 
 <script>
+import SignUpForm from '../components/SignUpForm.vue'
+
 export default {
-    name:"SignUpView"
+    name:"SignUpView",
+    components:{
+        SignUpForm
+    }
 }
 </script>
 
