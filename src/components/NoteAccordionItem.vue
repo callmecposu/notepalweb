0835d76fa38dbb300b6584af65b3ecbf6c4cae16
callmecposu <pc.cposu@gmail.com>
2023-07-20 19:50:04 -0400

    add Note Saving feature and changes tracker

--- src/components/NoteAccordionItem.vue
+++ src/components/NoteAccordionItem.vue
@@ -3,25 +3,25 @@
     <h2 class="accordion-header">
       <button
         class="accordion-button"
-        :class="{collapsed: !show}"
+        :class="{ collapsed: !show }"
         type="button"
         data-bs-toggle="collapse"
         :data-bs-target="'#' + noteID"
         aria-expanded="true"
         :aria-controls="noteID"
       >
-       {{ title }}
+        {{ title }} {{ (changed) ? ' *' : ''}}
       </button>
     </h2>
     <div
       :id="noteID"
       class="accordion-collapse collapse"
-      :class="{show: show}"
+      :class="{ show: show }"
     >
       <div class="accordion-body">
         <div class="form">
           <textarea
-          v-model="noteContent"
+            v-model="noteContent"
             class="form-control"
             placeholder="I feel so empty..."
             :id="'textArea_' + noteID"
@@ -30,10 +30,10 @@
         </div>
         <!-- note controls -->
         <div class="container mt-2 p-3">
-            <div class="btn btn-outline-success mx-2 mb-2">Save</div>
-            <div class="btn btn-outline-danger mx-2 mb-2">Delete</div>
-            <div class="btn btn-primary mx-2 mb-2">Share</div>
-            <div class="btn btn-secondary mx-2 mb-2">Remove</div>
+          <div class="btn btn-outline-success mx-2 mb-2" @click="saveNote">Save</div>
+          <div class="btn btn-outline-danger mx-2 mb-2">Delete</div>
+          <div class="btn btn-primary mx-2 mb-2">Share</div>
+          <div class="btn btn-secondary mx-2 mb-2">Remove</div>
         </div>
       </div>
     </div>
@@ -42,29 +42,50 @@
 
 <script>
 export default {
-    data(){
-        return{
-            noteContent: this.content
-        }
+  data() {
+    return {
+      noteContent: this.content,
+      lastSaveContent: this.content,
+      changed: false
+    };
+  },
+  props: ["noteID", "title", "content", "show"],
+  methods: {
+    async saveNote() {
+      const res = await fetch("http://localhost:8888/api/save_note", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        mode: "cors",
+        body: JSON.stringify({ noteID: this.noteID, content: this.noteContent }),
+      });
+      if (res.status == 200){
+        this.lastSaveContent = this.noteContent;
+        this.changed = false;
+      }
     },
-    props: ['noteID', 'title', 'content', 'show'],
+  },
+  watch:{
+    noteContent: function(){
+        this.changed = this.noteContent != this.lastSaveContent;
+    }
+  }
 };
 </script>
 
 <style scoped>
-.btn{
-    width: 80px;
+.btn {
+  width: 80px;
 }
-button.accordion-button{
-    background-color: rgb(227, 227, 227);
-    text-indent: 20px;
-    transition: background-color .1s ease-in-out;
-    transition: text-indent .1s ease-in-out;
+button.accordion-button {
+  background-color: rgb(227, 227, 227);
+  text-indent: 20px;
+  transition: background-color 0.1s ease-in-out;
+  transition: text-indent 0.1s ease-in-out;
 }
-button.accordion-button.collapsed{
-    text-indent: 0px;
-    background-color: whitesmoke;
-    transition: background-color .1s ease-in-out;
-    transition: text-indent .1s ease-in-out;
+button.accordion-button.collapsed {
+  text-indent: 0px;
+  background-color: whitesmoke;
+  transition: background-color 0.1s ease-in-out;
+  transition: text-indent 0.1s ease-in-out;
 }
 </style>
